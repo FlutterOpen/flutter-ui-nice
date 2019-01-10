7bfaaffe0a133f927e60d589e63fd6ff62cbec47
Edgar <ed@appsynth.net>
2019-01-10 12:52:42 +0700

    create coordinator and two main pages

---
+++ lib/page/navigation/navigation1/coordinator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Coordinator extends StatefulWidget {
+	@override
+	_Coordinator createState() => _Coordinator();
+}
+
+class _Coordinator extends State<Coordinator> {
+
+	@override
+  void initState() {
+    super.initState();
+  }
+	@override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
